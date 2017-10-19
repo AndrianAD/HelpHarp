96b7a83697c90f72cfc50faf58aa2f8981ff8266
AndrianAD <druc_andrian@mail.ru>
2017-10-19 14:31:34 +0300

    171019

--- app/src/main/java/com/example/user/helpharp/Activity2.java
+++ app/src/main/java/com/example/user/helpharp/Activity2.java
@@ -1,26 +1,21 @@
 package com.example.user.helpharp;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import static com.example.user.helpharp.MainActivity.check_temp;
-import static com.example.user.helpharp.MainActivity.harp2;
 
 public class Activity2 extends Activity {
-    TextView major, minor, blues, penta_minor;
+    TextView major, minor, blues, penta_minor, penta_major;
     static String gammaview = " ";
-    private Button button_ton;
     Harp harp = new Harp();
+    Harp harp2 = new Harp();
 
 
     @Override
@@ -34,21 +29,13 @@ public class Activity2 extends Activity {
         minor = (TextView) findViewById(R.id.gamma_minor);
         blues = (TextView) findViewById(R.id.gamma_blues);
         penta_minor = (TextView) findViewById(R.id.penta_minor);
-
-        button_ton = (Button) findViewById(R.id.button_ton);
-        button_ton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        penta_major = (TextView) findViewById(R.id.penta_major);
 
 
         // -------Spiner
         final Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
 // Настраиваем адаптер
-        ArrayAdapter<?> adapter =
-                ArrayAdapter.createFromResource(this, R.array.harmonica_key, android.R.layout.simple_spinner_item);
+        ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(this, R.array.harmonica_key, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Вызываем адаптер
         spinner2.setAdapter(adapter);
@@ -98,6 +85,7 @@ public class Activity2 extends Activity {
             }
 
             public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
@@ -114,55 +102,59 @@ public class Activity2 extends Activity {
                 getGamma_major(isChecked);
             }
         });
-//
-//// Чекбокс мінор
-//        CheckBox switchCheckBox_minor = (CheckBox) findViewById(R.id.switch_check_box_minor);
-//        switchCheckBox_minor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (noteList2.isEmpty()) {
-//                    return;
-//                }
-//                if (noteList.isEmpty()) {
-//                    noteList = (ArrayList) noteList2.clone();
-//                }
-//                getGamma_minor(isChecked);
-//            }
-//        });
-//
-//// Чекбокс блюз
-//        CheckBox switchCheckBox_blues = (CheckBox) findViewById(R.id.switch_check_box_blues);
-//        switchCheckBox_blues.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (noteList2.isEmpty()) {
-//                    return;
-//                }
-//                if (noteList.isEmpty()) {
-//                    noteList = (ArrayList) noteList2.clone();
-//                }
-//                getGamma_blues(isChecked);
-//            }
-//        });
-//
-//        // Чекбокс минорная пентатоника
-//        CheckBox switchCheckBox_penta_minor = (CheckBox) findViewById(R.id.checkBox_penta_minor);
-//        switchCheckBox_penta_minor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (noteList2.isEmpty()) {
-//                    return;
-//                }
-//                if (noteList.isEmpty()) {
-//                    noteList = (ArrayList) noteList2.clone();
-//                }
-//                get_minor_pentatonic(isChecked);
-//            }
-//        });
-//
-//
-//
-//
+
+// Чекбокс мінор
+        CheckBox switchCheckBox_minor = (CheckBox) findViewById(R.id.switch_check_box_minor);
+        switchCheckBox_minor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (harp.allnote.isEmpty()) {
+                    return;
+                }
+                getGamma_minor(isChecked);
+            }
+        });
+
+// Чекбокс блюз
+        CheckBox switchCheckBox_blues = (CheckBox) findViewById(R.id.switch_check_box_blues);
+        switchCheckBox_blues.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (harp.allnote.isEmpty()) {
+                    return;
+                }
+                getGamma_blues(isChecked);
+            }
+        });
+
+        // Чекбокс минорная пентатоника
+        CheckBox switchCheckBox_penta_minor = (CheckBox) findViewById(R.id.checkBox_penta_minor);
+        switchCheckBox_penta_minor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (harp.allnote.isEmpty()) {
+                    return;
+                }
+                get_minor_pentatonic(isChecked);
+            }
+        });
+
+        // Чекбокс мажорная пентатоника
+        CheckBox switchCheckBox_penta_major = (CheckBox) findViewById(R.id.checkBox_penta_major);
+        switchCheckBox_penta_major.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (harp.allnote.isEmpty()) {
+                    return;
+                }
+                get_major_pentatonic(isChecked);
+            }
+        });
+
+
+
+
+
     }
 
     public void getGamma_major(boolean isChecked) {
@@ -183,77 +175,81 @@ public class Activity2 extends Activity {
         major.setText(gammaview);
         gammaview = "";
     }
-//
-//    public static void getGamma_minor(boolean isChecked) {
-//        int j = 0;
-//        int int_masiv = 0;
-//        final int[] masiv = {2, 1, 2, 2, 1, 2, 2};
-//        for (int i = 0; i < 37 - temp; i = i + int_masiv) {
-//            int_masiv = masiv[j];
-//            Hole nots = (Hole) noteList2.get(i);
-//            Hole tabs = (Hole) noteList2.get(i + temp);
-//            j++;
-//            if (j == 7) {
-//                j = 0;
-//            }
-//            gammaview += isChecked ? nots.getNote() + " " : tabs.getTabs() + " ";
-//        }
-//        minor.setText(gammaview);
-//        gammaview = "";
-//    }
-//
-//    public static void getGamma_blues(boolean isChecked) {
-//        int j = 0;
-//        int int_masiv = 0;
-//        final int[] masiv = {3, 2, 1, 1, 3, 2};
-//        for (int i = 0; i < 37 - temp; i = i + int_masiv) {
-//            int_masiv = masiv[j];
-//            Hole nots = (Hole) noteList2.get(i);
-//            Hole tabs = (Hole) noteList2.get(i + temp);
-//            j++;
-//            if (j == 6) {
-//                j = 0;
-//            }
-//            gammaview += isChecked ? nots.getNote() + " " : tabs.getTabs() + " ";
-//        }
-//        blues.setText(gammaview);
-//        gammaview = "";
-//    }
-//
-//    public static void get_minor_pentatonic(boolean isChecked) {
-//        int j = 0;
-//        int int_masiv = 0;
-//        final int[] masiv = {3, 2, 2, 3, 2};
-//        for (int i = 0; i < 37 - temp; i = i + int_masiv) {
-//            int_masiv = masiv[j];
-//            Hole nots = (Hole) noteList2.get(i);
-//            Hole tabs = (Hole) noteList2.get(i + temp);
-//            j++;
-//            if (j == 5) {
-//                j = 0;
-//            }
-//            gammaview += isChecked ? nots.getNote() + " " : tabs.getTabs() + " ";
-//        }
-//        penta_minor.setText(gammaview);
-//        gammaview = "";
-//    }
-//
-//    public static void get_major_pentatonic(boolean isChecked) {
-//        int j = 0;
-//        int int_masiv = 0;
-//        final int[] masiv = {2, 2, 3, 2, 3};
-//        for (int i = 0; i < 37 - temp; i = i + int_masiv) {
-//            int_masiv = masiv[j];
-//            Hole nots = (Hole) noteList2.get(i);
-//            Hole tabs = (Hole) noteList2.get(i + temp);
-//            j++;
-//            if (j == 5) {
-//                j = 0;
-//            }
-//            gammaview += isChecked ? nots.getNote() + " " : tabs.getTabs() + " ";
-//        }
-//        blues.setText(gammaview);
-//        gammaview = "";
-//    }
+
+    public void getGamma_minor(boolean isChecked) {
+        int j = 0;
+        int int_masiv = 0;
+        int temp = check_temp(harp.position, harp2.position);
+        final int[] masiv = {2, 1, 2, 2, 1, 2, 2};
+        for (int i = 0; i < 37 - temp; i = i + int_masiv) {
+            int_masiv = masiv[j];
+            Hole nots = (Hole) harp.allnote.get(i + temp);
+            Hole tabs = (Hole) harp.allnote.get(i + temp);
+            j++;
+            if (j == 7) {
+                j = 0;
+            }
+            gammaview += isChecked ? nots.getNote() + " " : tabs.getTabs() + " ";
+        }
+        minor.setText(gammaview);
+        gammaview = "";
+    }
+
+    public void getGamma_blues(boolean isChecked) {
+        int j = 0;
+        int int_masiv = 0;
+        int temp = check_temp(harp.position, harp2.position);
+        final int[] masiv = {3, 2, 1, 1, 3, 2};
+        for (int i = 0; i < 37 - temp; i = i + int_masiv) {
+            int_masiv = masiv[j];
+            Hole nots = (Hole) harp.allnote.get(i + temp);
+            Hole tabs = (Hole) harp.allnote.get(i + temp);
+            j++;
+            if (j == 6) {
+                j = 0;
+            }
+            gammaview += isChecked ? nots.getNote() + " " : tabs.getTabs() + " ";
+        }
+        blues.setText(gammaview);
+        gammaview = "";
+    }
+
+    public void get_minor_pentatonic(boolean isChecked) {
+        int j = 0;
+        int int_masiv = 0;
+        int temp = check_temp(harp.position, harp2.position);
+        final int[] masiv = {3, 2, 2, 3, 2};
+        for (int i = 0; i < 37 - temp; i = i + int_masiv) {
+            int_masiv = masiv[j];
+            Hole nots = (Hole) harp.allnote.get(i + temp);
+            Hole tabs = (Hole) harp.allnote.get(i + temp);
+            j++;
+            if (j == 5) {
+                j = 0;
+            }
+            gammaview += isChecked ? nots.getNote() + " " : tabs.getTabs() + " ";
+        }
+        penta_minor.setText(gammaview);
+        gammaview = "";
+    }
+
+    public void get_major_pentatonic(boolean isChecked) {
+        int j = 0;
+        int int_masiv = 0;
+        int temp = check_temp(harp.position, harp2.position);
+        final int[] masiv = {2, 2, 3, 2, 3};
+        for (int i = 0; i < 37 - temp; i = i + int_masiv) {
+            int_masiv = masiv[j];
+            Hole nots = (Hole) harp.allnote.get(i + temp);
+            Hole tabs = (Hole) harp.allnote.get(i + temp);
+            j++;
+            if (j == 5) {
+                j = 0;
+            }
+            gammaview += isChecked ? nots.getNote() + " " : tabs.getTabs() + " ";
+        }
+        penta_major.setText(gammaview);
+        gammaview = "";
+    }
 
 }
